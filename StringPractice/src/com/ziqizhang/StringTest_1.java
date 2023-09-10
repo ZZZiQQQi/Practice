0623663dba2a4148a52e61e7abc6503e6c06a816
fuck <fuck@qq.com>
2023-09-10 21:30:17 +0800

    第一次提交并推送到GitHub仓库

---
+++ StringPractice/src/com/ziqizhang/StringTest_1.java
@@ -0,0 +1,14 @@
+package com.ziqizhang;
+
+/**
+ * ClassName:StringTest_1
+ * Package:com.ziqizhang
+ * Date:10/09/2023 9:26 PM
+ * Description:
+ * Author:1163070713@qq.com
+ */
+public class StringTest_1 {
+    public static void main(String[] args) {
+        System.out.println("哈哈哈");
+    }
+}

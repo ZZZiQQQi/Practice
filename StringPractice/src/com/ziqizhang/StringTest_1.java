78e6c82c2d77f2cb15b7a1f8f371e53d7507c009
ZZZiQQQi <125474869+ZZZiQQQi@users.noreply.github.com>
2023-09-11 14:11:42 +0800

    测试冲突

--- StringPractice/src/com/ziqizhang/StringTest_1.java
+++ StringPractice/src/com/ziqizhang/StringTest_1.java
@@ -17,5 +17,7 @@ public class StringTest_1 {
         System.out.println("模拟远程仓库main分支经过修过修改,然后用本地分支merge主分支");
 
         System.out.println("feature2分支进行修改");
+
+        System.out.println("冲突发生在local分支并未更新而远程项目已更新的情况下");
     }
 }

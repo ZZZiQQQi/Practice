db52eb7ce9b941dcb2e7606f1f7c74832f572229
ZZZiQQQi <125474869+ZZZiQQQi@users.noreply.github.com>
2023-09-11 13:36:14 +0800

    模拟远程main分支修改，本地feature1分支merge

--- StringPractice/src/com/ziqizhang/StringTest_1.java
+++ StringPractice/src/com/ziqizhang/StringTest_1.java
@@ -13,5 +13,7 @@ public class StringTest_1 {
         System.out.println("呵呵");
         System.out.println("嘻嘻");
         System.out.println("嗯嗯");
+
+        System.out.println("模拟远程仓库main分支经过修过修改,然后用本地分支merge主分支");
     }
 }

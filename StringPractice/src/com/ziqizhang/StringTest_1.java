f655ba3f4d3e7629c44b10adcaff5387454afe61
zhangziqi <1163070713@qq.com>
2023-09-11 13:49:26 +0800

    从main 分支 新开 分支 feature2分支 并进行修改

--- StringPractice/src/com/ziqizhang/StringTest_1.java
+++ StringPractice/src/com/ziqizhang/StringTest_1.java
@@ -11,9 +11,11 @@ public class StringTest_1 {
     public static void main(String[] args) {
         System.out.println("哈哈哈");
         System.out.println("呵呵");
-        System.out.println("嘻嘻");
+        System.out.println("feature2分支进行修改");
         System.out.println("嗯嗯");
 
         System.out.println("模拟远程仓库main分支经过修过修改,然后用本地分支merge主分支");
+
+        System.out.println("feature2分支进行修改");
     }
 }
